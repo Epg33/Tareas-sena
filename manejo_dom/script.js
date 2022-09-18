d3bbd2f47e1056d392d82dcadd5ca75efacfbe14
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-18 14:33:29 -0500

    corregir manejo del dom

--- manejo_dom/script.js
+++ manejo_dom/script.js
@@ -1,6 +1,27 @@
-const change = document.getElementById('change');
+const change = document.getElementById("change");
+const main = document.getElementById("main");
 
-change.addEventListener('click', ()=>{
-    const color = document.getElementById('color').value;
-    document.getElementById('background').style.background= `${color}`;
+
+change.addEventListener("click", () => {
+  const color = document.getElementById("color").value;
+  document.getElementById("background").style.background = `${color}`;
 });
+
+let radius_less = 0;
+let radius_more = 0;
+
+const disminuir = () => {
+  if (document.getElementsByTagName("main").style.borderRadius != "0") {
+    radius_less -= 1;
+    document.querySelector(
+      ".main-content"
+    ).style.borderRadius = `${radius_less}px;`;
+  }
+};
+
+const aumentar = () => {
+  radius_more += 1;
+  console.log(radius_more);
+  main.style.borderRadius = `${radius_more}px;`;
+  document.getElementById("background").style.borderRadius = `${radius_more}px;`;
+};
