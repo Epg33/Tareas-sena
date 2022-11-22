7710ce996dcab173f505663a046137f32569e32a
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-22 12:56:51 -0500

    arreglar sintaxis

--- base de datos/triggers.sql
+++ base de datos/triggers.sql
@@ -11,27 +11,16 @@ create table persona(
     ciudad varchar(20) not null
 );
 
-insert into persona (id, nombre, apellido, email, ciudad) values (1, "alex","lopez", "a@gmail.com","cali");
-
-
 delimiter //
 create procedure sp_registro_persona (id_p int, nombre_p varchar(15), apellido_p varchar(15), email_p varchar(30), ciudad_p varchar(20))
 begin 
 	insert into persona (id, nombre, apellido, email, ciudad) values (id_p, nombre_p, apellido_p, email_p, ciudad_p);
-end; //
-delimiter;
+end//
 
-call sp_registro_persona(2, "alexxx","lopez", "a@gmail.com","cali");
+call sp_registro_persona (1, 'alex', 'lopex', 'xd', 'szs');
 
-DELIMITER // 
-create trigger registrar_persona_bi before insert on persona for each row
+delimiter //
+create procedure sp_listar_persona ()
 begin
-declare id_p int;
-declare nombre_p varchar(15);
-declare apellido_p varchar(15);
-declare email_p varchar(30);
-declare ciudad_p varchar(20);
-insert into persona(id, nombre, apellido, email, ciudad) values(id_p, nombre_p, apellido_p, email_p, ciudad_p);
-end; //
-
-call sp_registro_persona(3, "alexxx","lopezzzz", "a@gmail.com","cali");
+	select * from persona;
+end//
