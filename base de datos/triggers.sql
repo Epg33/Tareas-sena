28268f551dd2da35023a3e4855ad5a3996d4390b
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-22 12:57:12 -0500

    arreglar sintaxis

--- base de datos/triggers.sql
+++ base de datos/triggers.sql
@@ -3,6 +3,12 @@ create database triggers_try;
 
 use triggers_try;
 
+create table logueos(
+	id int primary key auto_increment,
+    mensaje varchar(100) not null,
+    fecha datetime null default current_timestamp
+);
+
 create table persona(
 	id int primary key,
     nombre varchar(15) not null,
@@ -11,16 +17,26 @@ create table persona(
     ciudad varchar(20) not null
 );
 
+delimiter//
+create trigger after_insert
+	after insert on persona
+    for each row begin
+	insert into logueos(mensaje) values ('Se ha insertado en persona');
+end//
+
+
 delimiter //
 create procedure sp_registro_persona (id_p int, nombre_p varchar(15), apellido_p varchar(15), email_p varchar(30), ciudad_p varchar(20))
 begin 
 	insert into persona (id, nombre, apellido, email, ciudad) values (id_p, nombre_p, apellido_p, email_p, ciudad_p);
 end//
 
-call sp_registro_persona (1, 'alex', 'lopex', 'xd', 'szs');
+call sp_registro_persona (2, 'alex', 'lopex', 'xd', 'szs');
 
 delimiter //
 create procedure sp_listar_persona ()
 begin
 	select * from persona;
 end//
+
+call sp_listar_persona;
