d92a945f08e920ecfeb829e41d884dac696f09d0
EthiemGuerrero <ethiemalex03@gmail.com>
2023-02-16 21:03:18 -0500

    terminar

--- base de datos/taller_procedimientos.sql
+++ base de datos/taller_procedimientos.sql
@@ -193,7 +193,9 @@ call payment_operations('PayPal', @minimo, @maximo, @medio, @suma, @pagos);
 select @minimo as 'pago minimo', @maximo as 'pago maximo', @medio as 'promedio', @suma as 'suma de todos los pagos ', @pagos as 'cantidad de pagos realizados';
 
 /*4.Crea una base de datos llamada procedimientos que contenga una tabla llamada cuadrados. La tabla cuadrados debe tener dos columnas de tipo INT UNSIGNED, una columna llamada número y otra columna llamada cuadrado.
-Una vez creada la base de datos y la tabla deberá crear un procedimiento llamado calcular_cuadrados con las siguientes características. El procedimiento recibe un parámetro de entrada llamado tope de tipo INT UNSIGNED y calculará el valor de los cuadrados de los primeros números naturales hasta el valor introducido como parámetro. El valor del números y de sus cuadrados deberán ser almacenados en la tabla cuadrados que hemos creado previamente.
+Una vez creada la base de datos y la tabla deberá crear un procedimiento llamado calcular_cuadrados con las siguientes características.
+ El procedimiento recibe un parámetro de entrada llamado tope de tipo INT UNSIGNED y calculará el valor de los cuadrados de los primeros números naturales hasta el valor introducido como parámetro.
+ El valor del números y de sus cuadrados deberán ser almacenados en la tabla cuadrados que hemos creado previamente.
 Tenga en cuenta que el procedimiento deberá eliminar el contenido actual de la tabla antes de insertar los nuevos valores de los cuadrados que va a calcular.
 Utilice un bucle WHILE para resolver el procedimiento.*/
 create database procedimientos;
@@ -209,7 +211,7 @@ drop procedure if exists calcular_cuadrados;
 create procedure calcular_cuadrados(in tope int unsigned)
 begin 
 	declare counter int default 0;
-	delete from cuadrados;
+	delete from cuadrados where cuadrados.numero>=0;
 	while counter < tope do 
 		insert into cuadrados(numero, cuadrado) values (counter, counter*counter);
 		set counter = counter +1;
@@ -220,13 +222,63 @@ DELIMITER ;
 
 call calcular_cuadrados(10);
 select * from cuadrados;
-/*5.	Utilice un bucle REPEAT para resolver el procedimiento del ejercicio anterior.
-6.	Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.
-7.	Crea una base de datos llamada procedimientos que contenga una tabla llamada ejercicio. La tabla debe tener una única columna llamada número y el tipo de dato de esta columna debe ser INT UNSIGNED.
+
+
+/*5.	Utilice un bucle REPEAT para.
+ resolver el procedimiento del ejercicio anterior.*/
+DELIMITER $$
+drop procedure if exists calcular_cuadrados;
+create procedure calcular_cuadrados(in tope int unsigned)
+begin
+	declare counter int default 0;
+    set counter = 0;
+        repeat 
+		insert into cuadrados(numero, cuadrado) values (counter, counter*counter);
+        set counter = counter + 1;
+		until counter > tope end repeat;
+    end;
+$$
+
+DELIMITER ;
+
+call calcular_cuadrados(10);
+select * from cuadrados;    
+
+/*6.	Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.*/
+DELIMITER $$
+drop procedure if exists calcular_cuadrados;
+create procedure calcular_cuadrados(in tope int unsigned)
+begin
+declare counter int default 0;
+declare str varchar (100);
+set counter = 0;
+set str = '';
+	label1: loop
+		  insert into cuadrados(numero, cuadrado) values (counter, counter*counter);   
+        if counter >= 10 then 
+        leave label1;
+        end if;
+        
+        set str = concat(str,counter,',');
+        set counter = counter + 1;
+        iterate label1;
+   
+	end loop;
+   
+    end;
+$$
+
+DELIMITER ;
+
+call calcular_cuadrados(10);
+select * from cuadrados;
+
+
+/*7.	Crea una base de datos llamada procedimientos que contenga una tabla llamada ejercicio. La tabla debe tener una única columna llamada número y el tipo de dato de esta columna debe ser INT UNSIGNED.
 Una vez creada la base de datos y la tabla deberá crear un procedimiento llamado calcular_números con las siguientes características. El procedimiento recibe un parámetro de entrada llamado valor_inicial de tipo INT UNSIGNED y deberá almacenar en la tabla ejercicio toda la secuencia de números desde el valor inicial pasado como entrada hasta el 1.
 Tenga en cuenta que el procedimiento deberá eliminar el contenido actual de las tablas antes de insertar los nuevos valores.
-Utilice un bucle WHILE para resolver el procedimiento.
-8.	Utilice un bucle REPEAT para resolver el procedimiento del ejercicio anterior.
+Utilice un bucle WHILE para resolver el procedimiento.*/
+/*8.	Utilice un bucle REPEAT para resolver el procedimiento del ejercicio anterior.
 9.	Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.
 10.	Crea una base de datos llamada procedimientos que contenga una tabla llamada pares y otra tabla llamada impares. Las dos tablas deben tener única columna llamada número y el tipo de dato de esta columna debe ser INT UNSIGNED.
 Una vez creada la base de datos y las tablas deberá crear un procedimiento llamado calcular_pares_impares con las siguientes características. El procedimiento recibe un parámetro de entrada llamado tope de tipo INT UNSIGNED y deberá almacenar en la tabla pares aquellos números pares que existan entre el número 1 el valor introducido como parámetro. Habrá que realizar la misma operación para almacenar los números impares en la tabla impares.
